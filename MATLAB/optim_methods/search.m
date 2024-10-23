955bec0c871e1be3ade9d29c5c724eb02ab4d3fe
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-10-23 09:34:36 -0400

    Cleaning up exhaustive search code

--- MATLAB/optim_methods/search.m
+++ MATLAB/optim_methods/search.m
@@ -6,27 +6,27 @@ windows = windows-1;
 nt      = 48;
 N       = 8;
 addpath('../../utils/')
-    
+
 tic
 for window=windows
     window
-    system('rm output_*');
-    s1=strjoin({'awk -f "night_filt.awk"',num2str(window),'1'});
+    system('rm output_*'); % todo add Nfine on next line
+    s1=strjoin({'awk -f "necklace_filt.awk"',num2str(window),'1'});
     s2=strjoin({'cNecks_48_',num2str(N),'.txt > sols_temp.txt'},'');
-    system(strjoin({s1,s2}));    
-    
+    system(strjoin({s1,s2}));
+
     best_eig = -Inf;
     fileList = dir('output_*.txt');
 
     for ff=1:length(fileList)
-        
+
         % read into matlab
         fileID       = fopen(fileList(ff).name, 'r');
         data         = textscan(fileID, '%s');
         fclose(fileID);
         charData     = char(data{1});
         Bmat         = charData - '0';  % Subtract '0' to convert char '0'/'1' to numeric 0/1
-        
+
         % construct time matrix
         tvec = (1:nt)/nt - 1/nt;
         Tmat = repmat(tvec,size(Bmat,1),1);
@@ -35,7 +35,7 @@ for window=windows
         for ii=1:size(Smat,1)
             Smat(ii,:) = Tmat(ii,find(Tmat(ii,:)));
         end
-        
+
         % find optimal
         [~,eig]  = getMinEigMulti(Smat',1,1,1,false);
         if max(eig)>best_eig
@@ -44,14 +44,14 @@ for window=windows
             bopt            = Bmat(mind,:);
         end
     end
-    
+
     % compare to alternate practice of equispaced
     %polarplot(2*pi*topt,1,'.k')
     t_unif = ((1:N)/N-1/N)';
     fprintf("\nAlternative:  %f\n",getMinEig(t_unif,1));
     fprintf('Optimal    :  %f\n',max(eig));
-    
-    clear Bmat Smat Tmat 
+
+    clear Bmat Smat Tmat
     Bmat_master = [Bmat_master; bopt];
 end
 toc
