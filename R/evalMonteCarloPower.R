c2746d517b5428875fd5d2b48730e140b4743e4e
Turner Silverthorne <turner.silverthorne@gmail.com>
2024-11-02 11:03:20 -0400

    Removing outdated permutation test code

--- R/evalMonteCarloPower.R
+++ R/evalMonteCarloPower.R
@@ -12,14 +12,11 @@
 #' @param param$acro phase of signal in radians
 #' @param Nmc number of Monte Carlo samples
 #' @param alpha type I error, default value \code{alpha=.05}
-#' @param method set to \code{method='Ftest'} for F-test or \code{method='perm'} for
-#' permutation based hypothesis test
-#' @param Nperm number of permutations to use for permutation based test
 #'
 #' @return Monte Carlo estimate of power
 #' @author Turner Silverthorne
 #' @export
-evalMonteCarloPower<-function(tvec,param,Nmc,alpha=.05,method=c('Ftest','perm'),Nperm=1e2){
+evalMonteCarloPower<-function(tvec,param,Nmc,alpha=.05){
   method = match.arg(method)
 
   Amp  = param$Amp
@@ -28,21 +25,6 @@ evalMonteCarloPower<-function(tvec,param,Nmc,alpha=.05,method=c('Ftest','perm'),
   Ydat = t(replicate(Nmc,{Amp*cos(2*pi*freq*tvec -acro) + rnorm(length(tvec))}))
   if (method=='Ftest'){
     pwr  = mean(matrixTests::row_cosinor(Ydat,tvec,1/freq)$pvalue < alpha)
-  }else if(method=='perm'){
-    #stop('perm not implemented')
-    pvec_true = matrixTests::row_cosinor(Ydat,tvec,1/freq)$pvalue
-    perm_mat  = matrix(rep(NaN,Nmc*Nperm),nrow=Nmc,ncol = Nperm)
-    for (perm in c(1:Nperm)){
-      Pdat = matrix(rep(NaN,Nmc*length(tvec)),nrow=Nmc,ncol = length(tvec))
-      for (ii in c(1:Nmc)){
-        Pdat[ii,] = Ydat[ii,sample(1:length(tvec))]
-      }
-      perm_mat[,perm]=matrixTests::row_cosinor(Pdat,tvec,1/freq)$pvalue
-    }
-    pmat_true = replicate(Nperm,pvec_true)
-    perm_mat < pmat_true
-    R = rowSums(perm_mat<pmat_true)
-    pwr =mean((R+1)/(Nperm+1) < alpha)
   }
   return(pwr)
 }
