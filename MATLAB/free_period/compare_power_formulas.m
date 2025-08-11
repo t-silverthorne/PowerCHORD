83bbfb84dc45d92018fcf7b880e8e1a9cb8000d3
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-11 10:35:13 -0400

    Minor

--- MATLAB/free_period/compare_power_formulas.m
+++ MATLAB/free_period/compare_power_formulas.m
@@ -28,9 +28,52 @@ for ll = 1:nloop
     rss_obs        = squeeze(rss_obs);
     pval           = [pval;mean(rss_perm<rss_obs,2)];
 end
-
+ean(pval<alpha)
 fprintf('Monte Carlo:  %d\n',mean(pval<alpha))
 
-%% compare min exact vs Monte Carlo for several freq
+%% rough compare min exact vs Monte Carlo across freqs
+rng('default')
+clf
+clear
+fmin=1;
+fmax=5;
+n     = 12;
+tt    = rand(n,1);
+Amp   = 1.5;
+alpha = .05;
+acro  = pi/2;
+
+%fminsearch(@(f) evalFtestPower(tt,f,acro,Amp,alpha),fmax)
+%[bad_freq,fval]=fminbnd(@(f) evalFtestPower(tt,f,acro,Amp,alpha),fmin,fmax,);
+
+Nfplt = 1e3;
+freqs = linspace(fmin,fmax,Nfplt);
+pwrs  = arrayfun(@(x) evalFtestPower(tt,x,acro,Amp,alpha),freqs);
+plot(freqs,pwrs)
+
+Nfreq = 100;
+nrep  = 10;
+nloop = 1000;
+Nperm = 5e3;
+fvec = linspace(fmin,fmax,5);
+ests = arrayfun(@(ff) locPwrEst(tt,acro,Amp,alpha,ff,fmin,fmax,...
+    Nfreq,nrep,Nperm,nloop),fvec);
+hold on
+plot(fvec,ests,'.k')
 
 
+function pwr=locPwrEst(tt,acro,Amp,alpha,ftrue,fmin,fmax,Nfreq,nrep,Nperm,nloop)
+pval =[];
+n = length(tt);
+for ll = 1:nloop
+    ll
+    y              = Amp*cos(2*pi*ftrue*tt-acro)+randn(n,1,1,nrep,1);
+    Yperm          = perm5d(y,Nperm);
+    [~,~,rss_obs]  = fitCosinorWindowFreqPaged(tt,y,fmin,fmax,Nfreq);
+    [~,~,rss_perm] = fitCosinorWindowFreqPaged(tt,Yperm,fmin,fmax,Nfreq);
+    rss_perm       = squeeze(rss_perm);
+    rss_obs        = squeeze(rss_obs);
+    pval           = [pval;mean(rss_perm<rss_obs,2)];
+end
+pwr=mean(pval<alpha);
+end
