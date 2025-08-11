6f37ec9b7b27e9590f37e174b4537ae167778072
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-11 12:14:28 -0400

    Tensor mask for contraction with perm group

---
+++ MATLAB/two_moment_method/getSymm4Tensor.m
@@ -0,0 +1,10 @@
+function [outputArg1,outputArg2] = getSymm4Tensor(n)
+%GETSYMM4TENSOR tensor corresponding to fourth moment of symmetric group
+
+for ii=1:n
+    for jj=1:n
+        for kk=1:n
+            for ll=1:n
+
+end
+
