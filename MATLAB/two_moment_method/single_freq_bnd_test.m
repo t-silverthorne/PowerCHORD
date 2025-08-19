42c5b0effc19f12b28b9d97303c4ba328dac55b3
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-19 14:51:40 -0400

    Testing two moment method

--- MATLAB/two_moment_method/single_freq_bnd_test.m
+++ MATLAB/two_moment_method/single_freq_bnd_test.m
@@ -1,7 +1,6 @@
-n = 24;
-
-tt    = rand(n,1);%linspace(0,1,n+1);
-%tt    = tt(1:end-1)';
+% test two moments bound on a single frequency design problem
+n     = 48;
+tt    = rand(n,1);
 
 X     = [ones(n,1) cos(2*pi*tt) sin(2*pi*tt)];
 H     = [0 1 0; 0 0 1];
@@ -10,22 +9,35 @@ L     = L'*L;
 Sigma = eye(n);
 
 th    = rand*2*pi;
-mu    = 10*X*[0 5*cos(th) 1.9*sin(th)]';
-alpha = .15;
+alpha = .05;
 
 T = getSymm4Mask_subtypes(n);
 
 thvals = linspace(0,2*pi,2^7+1);
 thvals = thvals(1:end-1);
 
-Amp   = 10;
-freqs = [1 2 3];
-
+% Amp   = 10;
+% freqs = [1 2 3];
+% 
+% clf
+% for freq=freqs
+%     bvals = arrayfun(@(th) get2mBound(L,Sigma,...
+%         X*[0 Amp*cos(freq*th) Amp*sin(freq*th)]',alpha,T),thvals);
+%     plot(thvals,bvals,'-k')
+%     hold on
+%     pause(1)
+% end
+%%
+Amps = [10 100 1000]
 clf
-for freq=freqs
+for Amp=Amps
     bvals = arrayfun(@(th) get2mBound(L,Sigma,...
-        X*[0 Amp*cos(freq*th) Amp*sin(freq*th)]',alpha,T),thvals);
+        X*[0 Amp*cos(th) Amp*sin(th)]',alpha,T),thvals);
     plot(thvals,bvals,'-k')
+    ylim([0,1])
     hold on
-    pause(1)
+    pause(.1)
+    ylim([0,1])
 end
+
+
