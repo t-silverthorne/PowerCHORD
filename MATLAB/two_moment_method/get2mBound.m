dcac08878d56ed73cee20b9587c100e927fb22b3
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-21 11:50:35 -0400

    Vectorized two-moment bound

--- MATLAB/two_moment_method/get2mBound.m
+++ MATLAB/two_moment_method/get2mBound.m
@@ -1,7 +1,6 @@
-function [bnd,sign_check] = get2mBound(L,Sigma,mu,alpha,T)
+function [bnd,sign_check] = get2mBound(L,Sigma,mu,alpha,Tlin)
 %GET2MBOUND lower bound on power from two moments method
-G = getIsserlisTensor(Sigma,mu); 
-[E_p,E_p_E_pi,E_p_Var_pi,Var_p,Var_p_E_pi] = getExactCenteredMoments(L,Sigma,mu,T);
+[E_p,E_p_E_pi,E_p_Var_pi,Var_p,Var_p_E_pi] = getExactCenteredMoments(L,Sigma,mu,Tlin);
 lhs        = E_p - E_p_E_pi;
 rhs        = sqrt(3*Var_p_E_pi) + sqrt(3*Var_p) + sqrt(3*E_p_Var_pi/alpha);
 bnd        = 1-(rhs/lhs).^2;
