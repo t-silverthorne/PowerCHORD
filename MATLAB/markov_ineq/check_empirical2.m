58d14ac5d98802275b7ee50b064cc83adae8c500
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-22 22:15:44 -0400

    Markov inequality experiment

---
+++ MATLAB/markov_ineq/check_empirical2.m
@@ -0,0 +1,21 @@
+clear
+rng('default')
+n     = 25;
+Amp   = 10;
+tt    = linspace(0,1,n+1);
+tt    = tt(1:end-1)';
+acro  = pi/2
+alpha = .05;
+freqs = linspace(1,1,1);
+Nsamp = 1e3;
+ftrue = 1;
+freqs = reshape(freqs,1,1,[]);
+[~,L] = getSpecPaged(tt,freqs);
+J     = ones(n,n)-eye(n);
+w1    = factorial(n-1)/factorial(n);
+w2    = factorial(n-2)/factorial(n);
+y     = Amp*cos(2*pi*ftrue*tt-acro) + randn(n,Nsamp);
+
+pbnd = (w1*trace(L)*diag(y'*y) + w2*trace(L*J)*diag(y'*J*y))./diag(y'*L*y);
+mean(pbnd<.05)
+mean(getPermPval(tt,y,freqs,1e3,'T2')<.05)
