8870ea12ad901172dab2a0239fe99c53919b3876
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-25 14:05:54 -0400

    Memory slicing for cheb bound

--- MATLAB/peer_review/figures/rand_chebBound.m
+++ MATLAB/peer_review/figures/rand_chebBound.m
@@ -1,7 +1,7 @@
 % check how Tinf statistic behaves on randomly generated design
 clear;clf;
 Nmeas = 16;
-Nfreq = 16;
+Nfreq = 20;
 Nacro = 16;
 Nperm = 1e2;
 Nsamp = 1e2;
@@ -9,7 +9,7 @@ addpath('../utils')
 
 fmin = 1;
 fmax = Nmeas/2;
-Amp  = 1.5;
+Amp  = 5;
 
 for ii=1:2
     if ii==2
@@ -36,22 +36,22 @@ for ii=1:2
     sz    = size(mu);
     x     = mu + randn([sz(1:4),Nsamp,sz(6:end)]);
     
+    [~,Tq] = getSymm4Mask_subtypes(Nmeas);
     % estimate power
     tic
-    pwr2   = squeeze(fastMCT2power(Q2,x,Nperm,0.05));
-    pwrinf = squeeze(fastMCTinfpower(Qinf,x,Nperm,0.05));
+    [pwr2,sgn] = evalChebPowerbnd(Q2,x,Tq,0.05,20);
+    pwr2 = pwr2.*((-1).^(~sgn));
+    pwr2 = squeeze(pwr2);
     toc
     
     if ii==1
         plot(squeeze(freqs),squeeze(min(pwr2,[],2)),'--k')
         hold on
-        plot(squeeze(freqs),squeeze(min(pwrinf,[],2)),'-k')
         drawnow
         ylim([0,1])
     else
         plot(squeeze(freqs),squeeze(min(pwr2,[],2)),'--b')
         hold on
-        plot(squeeze(freqs),squeeze(min(pwrinf,[],2)),'-b')
     end
-    ylim([0,1])
+    ylim([-1,1])
 end
--- MATLAB/peer_review/utils/evalChebPowerbnd.m
+++ MATLAB/peer_review/utils/evalChebPowerbnd.m
@@ -1,7 +1,29 @@
-function [pwrbnd,sgn] = evalChebPowerbnd(Q,x,Tq,alpha)
+function [pwrbnd,sgn] = evalChebPowerbnd(Q,x,Tq,alpha,sliceSize)
 %EVALCHEBPOWERBND Summary of this function goes here
 %   Detailed explanation goes here
-pbnd   = evalChebPbnd(Q,x,Tq);
+arguments
+    Q (:,:) double;
+    x double;
+    Tq  (:,:) logical;
+    alpha double;
+    sliceSize=Inf;
+end
+if sliceSize==Inf % no slicing
+    pbnd = evalChebPbnd(Q,x,Tq);
+else
+    sz = size(x); % with slicing
+    assert(mod(sz(5),sliceSize)==0,'dim 5 must be divisible by sliceSize');
+    nSlices = floor(sz(5)/sliceSize);
+    xrs = reshape(x,[sz(1:4),sliceSize,nSlices,sz(6:end)]);
+    
+    szrs = size(xrs);
+    pbnd = NaN([1,szrs(2:end)]); 
+    for kk=1:nSlices
+        pbnd(:,:,:,:,:,kk,:) = evalChebPbnd(Q,xrs(:,:,:,:,:,kk,:),Tq);
+    end
+    pbnd   = reshape(pbnd,[1,sz(2:end)]);
+
+end
 mp     = mean(pbnd,5);
 vp     = mean(pbnd.^2,5)-mp.^2;
 sgn    = mp<alpha;
