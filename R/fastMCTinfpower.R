88a0b63dfe5390fb532ce3d7ea5529e1ce2f2f60
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-08-28 11:29:01 -0400

    Tinfty test statistic in R

---
+++ R/fastMCTinfpower.R
@@ -0,0 +1,43 @@
+fastMCTinfpower = function(Q, x, Nperm, alpha) {
+  # Q: n × n × n3_Q
+  # x: n × n2 × n3 × n4
+  # Nperm: number of permutations
+  # alpha: significance level
+
+  sz = dim(x)
+  n = sz[1]
+  nTrailing = prod(sz[-1])
+  n3_Q = dim(Q)[3]
+
+  # flatten trailing dims for fast BLAS
+  x_flat = matrix(x, nrow = n, ncol = nTrailing)
+
+  # ---- observed test statistic ----
+  Tobs_vec = rep(-Inf, nTrailing)
+  for (k in seq_len(n3_Q)) {
+    Qx = Q[,,k] %*% x_flat
+    Tobs_vec = pmax(Tobs_vec, colSums(x_flat * Qx))
+  }
+  # reshape back to trailing dims
+  Tobs = array(Tobs_vec, dim = sz[-1])
+
+  count = array(0, dim = dim(Tobs))
+  for (pp in seq_len(Nperm)) {
+    # permute rows independently for each trailing column
+    perms = replicate(nTrailing, sample.int(n), simplify = TRUE)
+    idx = perms + rep((0:(nTrailing-1))*n, each = n)
+    xfp = x_flat[idx]
+    xfp = matrix(xfp, nrow = n, ncol = nTrailing)
+
+    Tperm_vec = rep(-Inf, nTrailing)
+    for (k in seq_len(n3_Q)) {
+      Qx = Q[,,k] %*% xfp
+      Tperm_vec = pmax(Tperm_vec, colSums(xfp * Qx))
+    }
+    Tperm = array(Tperm_vec, dim = sz[-1])
+    count = count + (Tperm > Tobs)
+  }
+  count   = count/Nperm
+  pwr_est = apply(count,dim(count)[-1],mean)
+  return(pwr_est)
+}
