1828a7f5e5a2ed301a5b29bcb44ac08ac7316a0e
Turner Silverthorne <tlsilver@uwaterloo.ca>
2025-09-05 12:49:06 -0400

    Simulated annealing with realistic grid

--- MATLAB/peer_review/figures/rand_chebBound4.m
+++ MATLAB/peer_review/figures/rand_chebBound4.m
@@ -1,7 +1,11 @@
+% Setup simulated annealing
+clear;clf;
+rng('default');
+addpath('../utils')
 Nmeas = 48;
-Nfreq = 16;
-Nacro = 16;
-Nsamp = 1e1;
+Nfreq = 64;
+Nacro = 64;
+Nsamp = 1e2;
 fmin  = 1;
 fmax  = Nmeas/4;
 Amp   = 5;
@@ -25,11 +29,14 @@ options = optimoptions('simulannealbnd', ...
     'MaxIterations',1e2, ...
     'MaxFunctionEvaluations',1e4);
 
+% Run simulated annealing
 [tt_opt,fval,exitflag,output] = simulannealbnd(Jwrap,tt0,lb,ub,options);
 
+
+% Benchmark result on higher resolution grid
 tt    = tt_opt
 Nfreq = 64;
-Nacro = 32;
+Nacro = 64;
 Nsamp = 1e2;
 Q2    = getQuadForm(tt,fqf_2);
 
@@ -46,7 +53,7 @@ x     = mu + randn([sz(1:4),Nsamp,sz(6:end)]);
 pwr2 = pwr2.*((-1).^(~sgn));
 pwr2 = squeeze(pwr2);
 plot(squeeze(freqs),squeeze(min(pwr2,[],2)),'--k','LineWidth',1)
-min(pwr2,'all')
+min(pwr2,[],'all')
 function Jval = Jfun(tt,freqs,acros,fqf_2,Amp,Nsamp)
 tt    = reshape(tt,[],1);
 Q2    = getQuadForm(tt,fqf_2);
