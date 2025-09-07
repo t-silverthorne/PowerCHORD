797abe42942bf0eec08d943460b3a9f7a526b010
Turner Silverthorne <turner.silverthorne@gmail.com>
2025-09-07 09:02:00 -0400

    Better benchmarking wrapper

---
+++ MATLAB/peer_review/utils/benchmarkDesign.m
@@ -0,0 +1,34 @@
+function [pwr2_mc,pwrinf_mc,pwr2_ch,freqs_mc,freqs_ch]=benchmarkDesign(...
+                         tt,fmin,fmax,Amp,...
+                         Nfreq_ch,Nacro_ch,Nsamp_ch,Nfq_Tinf,Nfq_T2, ...
+                         Nfreq_mc,Nacro_mc,Nsamp_mc,Nperm_mc)
+fqf_inf   = reshape(linspace(fmin,fmax,Nfq_Tinf),1,1,[]);
+fqf_2     = reshape(linspace(fmin,fmax,Nfq_T2),1,1,[]);
+[~,Qinf] = getQuadForm(tt,fqf_inf);
+[Q2,~] = getQuadForm(tt,fqf_2);
+
+% get MC estimates of T2 and Tinf power
+freqs_mc     = linspace(fmin,fmax,Nfreq_mc);
+acros     = linspace(0,2*pi,Nacro_mc+1);
+acros     = acros(1:end-1);
+freqs_mc     = reshape(freqs_mc,1,1,1,1,1,[]);
+acros     = reshape(acros,1,1,1,1,1,1,[]);
+mu        = Amp*cos(2*pi*freqs_mc.*tt -acros);
+sz        = size(mu);
+x         = mu + randn([sz(1:4),Nsamp_mc,sz(6:end)]);
+pwr2_mc   = min(squeeze(fastMCT2power(Q2,x,Nperm_mc,0.05)),[],2);
+pwrinf_mc = min(squeeze(fastMCTinfpower(Qinf,x,Nperm_mc,0.05)),[],2);
+
+% get Chebyshev bound on T2 power
+freqs_ch      = linspace(fmin,fmax,Nfreq_ch);
+acros         = rand(Nacro_ch,1)*2*pi;
+freqs_ch      = reshape(freqs_ch,1,1,1,1,1,[]);
+acros         = reshape(acros,1,1,1,1,1,1,[]);
+mu            = Amp*cos(2*pi*freqs_ch.*tt -acros);
+sz            = size(mu);
+x             = mu + randn([sz(1:4),Nsamp_ch,sz(6:end)]);
+[pwr2_ch,sgn] = evalChebPowerbnd(Q2,x,0.05,'rig');
+pwr2_ch       = min(squeeze(pwr2_ch.*((-1).^(~sgn))),[],2);
+freqs_mc = squeeze(freqs_mc);
+freqs_ch = squeeze(freqs_ch);
+end
--- MATLAB/peer_review/utils/getQuadForm.m
+++ MATLAB/peer_review/utils/getQuadForm.m
@@ -1,4 +1,4 @@
-function [Q,Qf] = getQuadForm(tt,freqs)
+function [Q2,Qinf] = getQuadForm(tt,freqs)
 %GETQUADFORM Summary of this function goes here
 %   Detailed explanation goes here
 H     = [0 1 0; 0 0 1];
@@ -6,7 +6,6 @@ n     = length(tt);
 X     = [ones(n,1,length(freqs)) cos(2*pi*freqs.*tt) sin(2*pi*freqs.*tt)];
 Q     = pagemldivide(pagemtimes(pagetranspose(X),X),pagetranspose(X));
 Q     = pagemtimes(H,Q);
-Qf    = pagemtimes(pagetranspose(Q),Q);
-Q     = mean(Qf,3);
+Qinf  = pagemtimes(pagetranspose(Q),Q);
+Q2    = mean(Qinf,3);
 end
-
